6bddc9faa87b29ba1e69ad466cf3389489921453
Maria Reedstrom <maria.reedstrom@gmail.com>
2022-08-15 11:09:48 -0500

    commit tree 46993c393f1195baf33fa393b07a472854ffba96

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,16 +2,17 @@ class PostsController < ApplicationController
 
   def show
     post = Post.find(params[:id])
-    
     render json: post
   end
 
   def update
     post = Post.find(params[:id])
-
     post.update(post_params)
-
+    if post.valid?
     render json: post
+    else
+    render json: { errors: post.errors }, status: :unprocessable_entity
+    end
   end
 
   private
@@ -19,5 +20,6 @@ class PostsController < ApplicationController
   def post_params
     params.permit(:category, :content, :title)
   end
+
 
 end
